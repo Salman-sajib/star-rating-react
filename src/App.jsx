e382aa86f05e0f98b4d27937da3b22a3ad00e62d
Salman-sajib <sfsajib@gmail.com>
2024-02-18 12:42:03 +0600

    wrote all the jsx and the functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
-import { CiStar } from 'react-icons/ci';
+import { FaStar } from 'react-icons/fa';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 function App() {
+  const numberOfStars = 5;
+
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClidk(index) {
+    setRating(index);
+  }
+
+  function handleMouseEnter(index) {
+    setHover(index);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
   return (
     <div className='app'>
-      <h1>Rate This</h1>
+      <h1 className='title'>Rate This</h1>
+      <h2 className='star-rating'>
+        {Array.from({ length: numberOfStars }).map(
+          (_, index) => (
+            (index += 1),
+            (
+              <FaStar
+                key={nanoid()}
+                style={{
+                  cursor: 'pointer',
+                }}
+                className={index <= (hover || rating) ? 'active' : 'inactive'}
+                onClick={() => handleClidk(index)}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={() => handleMouseLeave()}
+                size={30}
+              />
+            )
+          )
+        )}
+      </h2>
     </div>
   );
 }
