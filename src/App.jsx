f0b7064f9be82b4dac9b424bba20d03e6abd5a76
Salman-sajib <sfsajib@gmail.com>
2024-02-18 10:45:50 +0600

    initialize the project with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { CiStar } from 'react-icons/ci';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello <CiStar />{' '}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
